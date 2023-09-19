6fed686735969cf073e84ebbd551901475d0e66d
Bakdaulet Zhaksylyk <zhh_bakdaulet@mail.ru>
2023-09-19 11:18:17 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const changeColorButton = document.getElementById("changeColorButton");
+
+    // Array of background colors to cycle through
+    const colors = ["#ff5733", "#33ff57", "#5733ff", "#ff57e3"];
+
+    let currentColorIndex = 0;
+
+    // Function to change the background color
+    function changeBackgroundColor() {
+        document.body.style.backgroundColor = colors[currentColorIndex];
+        currentColorIndex = (currentColorIndex + 1) % colors.length;
+    }
+
+    // Add a click event listener to the button
+    changeColorButton.addEventListener("click", changeBackgroundColor);
+});
+    
